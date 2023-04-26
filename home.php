33635d41de66964e55aac690ea44cb3d9838c59b
Daniel <daniel_stef@abv.bg>
2023-04-26 22:51:12 +0200

    Daniel change

--- home.php
+++ home.php
@@ -1,57 +1,74 @@
 <?php
-// Database connection
-$mysqli = new mysqli("localhost", "root", "", "beauty_care");
+// Connect to the database using PDO
+$servername = "localhost";
+$username = "root";
+$dbname = "I_care";
 
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 
-// Fetch products from the database
-$product_query = "SELECT * FROM products";
-$product_result = $mysqli->query($product_query);
-
-// Fetch services from the database
-$service_query = "SELECT * FROM services";
-$service_result = $mysqli->query($service_query);
+// Retrieve the featured products from the database
+$sql = "SELECT * FROM products WHERE featured = 1";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$featured_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Beauty & Care</title>
-    <link rel="stylesheet" href="style.css"> <!-- Replace with your CSS file path -->
+    <title>Beauty Care Webshop</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1>Products</h1>
-<div class="product-container">
-    <?php
-    // Loop through products and display them
-    while ($product_row = $product_result->fetch_assoc()) {
-        echo '<div class="product">';
-        echo '<img src="' . $product_row["image"] . '" alt="' . $product_row["name"] . '">';
-        echo '<h2>' . $product_row["name"] . '</h2>';
-        echo '<p>' . $product_row["description"] . '</p>';
-        echo '<p>Price: $' . $product_row["price"] . '</p>';
-        echo '</div>';
-    }
-    ?>
-</div>
+<header>
+    <div class="container">
+        <h1>Beauty Care Webshop</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Men Care</a></li>
+                <li><a href="#">Women Care</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Sign in</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
-<h1>Services</h1>
-<div class="service-container">
-    <?php
-    // Loop through services and display them
-    while ($service_row = $service_result->fetch_assoc()) {
-        echo '<div class="service">';
-        echo '<img src="' . $service_row["image"] . '" alt="' . $service_row["name"] . '">';
-        echo '<h2>' . $service_row["name"] . '</h2>';
-        echo '<p>' . $service_row["description"] . '</p>';
-        echo '<p>Price: $' . $service_row["price"] . '</p>';
-        echo '</div>';
-    }
-    ?>
-</div>
+<main>
+    <section class="hero">
+        <div class="container">
+            <h2>Find Your Perfect Beauty Products</h2>
+            <p>Shop our range of high-quality beauty products that cater to your every need.</p>
+            <a href="#" class="btn btn-primary">Shop Now</a>
+        </div>
+    </section>
+
+    <section class="featured-products">
+        <div class="container">
+            <h3>Featured Products</h3>
+            <div class="products">
+                <?php foreach ($featured_products as $product): ?>
+                    <div class="product">
+                        <img src="<?php echo $product['image']; ?>">
+                        <h4><?php echo $product['name']; ?></h4>
+                        <p><?php echo '$' . $product['price']; ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </section>
+</main>
+
+<footer>
+    <div class="container">
+        <p>&copy; 2023 Beauty Care Webshop</p>
+    </div>
+</footer>
 </body>
 </html>
